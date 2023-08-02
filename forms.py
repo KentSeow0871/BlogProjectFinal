3a2eca96e46e2626251dc568079621ed2be2260d
Kent <skc0871.gmail.com>
2023-08-02 14:30:29 +0800

    Initial Commit

---
+++ forms.py
@@ -0,0 +1,34 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, PasswordField
+from wtforms.validators import DataRequired, URL
+from flask_ckeditor import CKEditorField
+
+
+# WTForm for creating a blog post
+class CreatePostForm(FlaskForm):
+    title = StringField("Blog Post Title", validators=[DataRequired()])
+    subtitle = StringField("Subtitle", validators=[DataRequired()])
+    img_url = StringField("Blog Image URL", validators=[DataRequired(), URL()])
+    body = CKEditorField("Blog Content", validators=[DataRequired()])
+    submit = SubmitField("Submit Post")
+
+
+# TODO: Create a form to register new users
+class RegisterForm(FlaskForm):
+    name = StringField(label="Name", validators=[DataRequired()])
+    email = StringField(label="Email", validators=[DataRequired()])
+    password = PasswordField(label="Password", validators=[DataRequired()])
+    submit = SubmitField("SIGN ME UP!")
+
+
+# Create a Login form to log in users
+class LoginForm(FlaskForm):
+    email = StringField(label="Email", validators=[DataRequired()])
+    password = PasswordField(label="Password", validators=[DataRequired()])
+    submit = SubmitField("LET ME IN!")
+
+
+# Create a comment form for users to leave comments
+class CommentForm(FlaskForm):
+    comment_text = CKEditorField(label="Comment", validators=[DataRequired()])
+    submit = SubmitField("SUBMIT COMMENT")
